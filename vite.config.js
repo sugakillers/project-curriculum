f511d195a34a440703f25e19f4b52bd90e9991cd
kamila <sugakillersxs@gmail.com>
2025-11-02 18:22:53 +0100

    add all

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+
+    base: '/project-curriculum/',
+
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
